d39025136d252999862ee1431d5b5cc0cf7e3e9c
oyarosevych <alexandr.yarosevich@gmail.com>
2015-11-25 15:44:59 +0200

    Redesigned UserDAO to use stored procedures

--- src/main/java/com/yarosevych/blog/dao/UserDao.java
+++ src/main/java/com/yarosevych/blog/dao/UserDao.java
@@ -1,6 +1,5 @@
 package com.yarosevych.blog.dao;
 
-import com.sun.xml.internal.bind.v2.TODO;
 import com.yarosevych.blog.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -18,42 +17,47 @@ public class UserDao {
 
     public void addUser(String nickname) throws SQLException {
         Connection connection = dataSource.getConnection();
-        PreparedStatement preparedStatement = connection.prepareStatement(
-                "INSERT INTO users (nickname) values (?)");
-        preparedStatement.setString(1, nickname);
-        preparedStatement.executeUpdate();
-        connection.close();
-    }
-
-//    TODO: 1. Switch to stored procedure in the whole class.
-    public User getUserById(Integer id) throws SQLException {
-        Connection connection = dataSource.getConnection();
         try {
-            PreparedStatement preparedStatement = connection.prepareStatement(
-                    "SELECT * FROM users WHERE id=?");
-            preparedStatement.setInt(1, id);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            resultSet.next();
-            Integer userId = resultSet.getInt(1);
-            String nickname = resultSet.getString(2);
-            return new User(userId, nickname);
+            CallableStatement callableStatement = connection.prepareCall("CALL addUser(?)");
+            callableStatement.setString(1, nickname);
+            callableStatement.executeUpdate();
         } finally {
             connection.close();
         }
+    }
 
-
+    public User getUserById(Integer id) throws SQLException {
+        Connection connection = dataSource.getConnection();
+        try {
+            CallableStatement callableStatement = connection.prepareCall("CALL getUserById(?)");
+            callableStatement.setInt(1, id);
+            ResultSet resultSet = callableStatement.executeQuery();
+            if (resultSet.next()) {
+                Integer userId = resultSet.getInt(1);
+                String nickname = resultSet.getString(2);
+                return new User(userId, nickname);
+            } else {
+                return null;
+            }
+        } finally {
+            connection.close();
+        }
     }
 
     public List<User> getAllUsers() throws SQLException {
         Connection connection = dataSource.getConnection();
         List<User> users = new ArrayList<>();
-        PreparedStatement preparedStatement = connection.prepareStatement(
-                "SELECT * FROM users");
-        ResultSet resultSet = preparedStatement.executeQuery();
-        while (resultSet.next()) {
-            users.add(new User(resultSet.getInt(1), resultSet.getString(2)));
+        try {
+            CallableStatement callableStatement = connection.prepareCall("CALL getAllUsers()");
+            ResultSet resultSet = callableStatement.executeQuery();
+            while (resultSet.next()) {
+                Integer userId = resultSet.getInt(1);
+                String nickname = resultSet.getString(2);
+                users.add(new User(userId, nickname));
+            }
+        } finally {
+            connection.close();
         }
-        connection.close();
         return users;
     }
 }
